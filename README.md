b48fabdaa39067185ef534cba91834d9c55ddc6e
Hungereeeeee <ycr1714336458@163.com>
2017-03-06 16:37:11 +0800

    新建项目

---
+++ README.md
@@ -0,0 +1,33 @@
+# music
+
+> A Vue.js project
+
+一个音乐app 初步打算暂时先只构建出界面，为了熟悉vue-cli和Vue Router的使用
+
+* 2017/3/6
+
+  写出底部导航栏，安装Vue Router，点击底部导航栏时切换到相应的页面
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <!--<header></header>-->
+    <footer>
+      <ul>
+        <li>
+          <router-link to="/discovery">
+             <span>
+              <i class="iconfont icon-wangyiyunyinlezizhi-copy"></i>
+            </span>
+            <span>发现音乐</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/mymusic">
+              <span>
+                <i class="iconfont icon-iconupload"></i>
+              </span>
+              <span>我的音乐</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/friends">
+            <span>
+              <i class="iconfont icon-19"></i>
+            </span>
+            <span>朋 友</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/account">
+            <span>
+             <i class="iconfont icon-account"></i>
+            </span>
+            <span>账 号</span>
+          </router-link>
+        </li>
+      </ul>
+    </footer>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import 'normalize.css'
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+*
+  box-sizing border-box
+html,body
+  font-size 10px
+#app
+  height  100vh
+  overflow hidden
+  position relative
+  header
+    background-color rgb(42,42,42)
+    width 100%
+    height 8rem
+    position fixed
+  footer
+    background-color rgb(180,11,11)
+    width 100%
+    height 8rem
+    position fixed
+    bottom 0
+    ul
+      padding-left 0
+      padding-top 1rem
+      text-align : center
+      margin 0
+      color white
+      font-size 1.6rem
+      li
+        display inline-block
+        width 24%
+        a
+          color white
+          text-decoration none
+          span
+            display block
+            .iconfont
+              font-size 4rem
+</style>
